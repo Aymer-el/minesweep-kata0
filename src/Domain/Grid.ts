30ab4f0e449e2685700df8880ebe5e320a5a68a3
Aymericus <fi.aymeric@pm.me>
2020-02-21 16:18:00 +0100

    challenge 2: preparing for 2:2 challenge: refactoring

--- src/Domain/Grid.ts
+++ src/Domain/Grid.ts
@@ -52,53 +52,30 @@ export class Grid {
               Both Algorithm: are having the same goal: adding surrounding mines. You can comment on on off.
           */
             if (cells[i].mine) {
-                // 0 à 9
-                let isTopOK = i >= column ? 1 : 0;
-                // 90
-                let isBottomOK = (!isTopOK
-                  ? 1
-                  : i < cells.length - column)
-                    ? 1
-                    : 0;
-                // 15 % 10 = 5 parcontre 10 % 10 = 0
-                let isLeftOK = i % column !== 0 ? 1 : 0;
-                // 19 + 1 % 5)
-                let isRightOK = (!isLeftOK
-                  ? 1
-                  : (i + 1) % column !== 0)
-                    ? 1
-                    : 0;
+                let top = i >= column;
+                let bottom = !top || i < cells.length - column;
+                let left = i % column !== 0;
+                let right = !left || (i + 1) % column !== 0;
                 // Mapping possible cells around the cell of the index, from 0 to 8.
                 let position = 0;
                 // Whether the direction in the grid is ok: left, top, right, bottom.
-                const truthTab: Array<number> = [
-                    isLeftOK,
-                    isTopOK,
-                    isRightOK,
-                    isBottomOK,
-                    isLeftOK,
-                ];
+                const truthTab: Array<boolean> = [left, top, right, bottom, left];
                 // Cells to count or decount Around the current index (i).
                 const coordinate: Array<number> = [-1, -10];
                 // is two possible the two direction possible L largeur et l longueur ?
                 let isPair: boolean = true;
                 while (position < 8) {
                     isPair = position % 2 === 0;
-                    if (isPair) {
-                        cells[
-                            i +
-                                coordinate[0] *
-                                    truthTab[Math.floor(position / 2)]
-                        ].surroundingMines++;
-                    } else {
-                        cells[
-                            i +
-                                (coordinate[0] + coordinate[1]) *
-                                    (truthTab[Math.floor(position / 2)] *
-                                        truthTab[
-                                            Math.floor((position + 1) / 2)
-                                        ])
-                        ].surroundingMines++;
+                    if (isPair && truthTab[Math.floor(position / 2)]) {
+                        cells[i + coordinate[0]].surroundingMines++;
+                    } else if (
+                        truthTab[Math.floor(position / 2)] &&
+                        truthTab[Math.floor((position + 1) / 2)]
+                    ) {
+                        cells[i + (coordinate[0] + coordinate[1])]
+                            .surroundingMines++;
+                    }
+                    if (!isPair) {
                         coordinate.push(-coordinate[0]);
                         coordinate.shift();
                     }
